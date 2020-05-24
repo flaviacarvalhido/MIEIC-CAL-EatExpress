1574c40757fdcac6cbcedf9d58285abd48c5b81f
joao0903 <joao.goncalves0903@gmail.com>
2020-05-24 22:35:39 +0100

    merging is dogshit

--- Parse.h
+++ Parse.h
@@ -4,5 +4,26 @@
 
 #ifndef EATEXPRESS_PARSE_H
 #define EATEXPRESS_PARSE_H
+#pragma once
+
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include "Utils.h"
+#include "Point.h"
+#include "Graph.h"
+#include <GraphViewer/graphviewer.h>
+
+
+using namespace std;
+
+void parseNodes(const string path_to_nodes);
+void parseEdges(const string path_to_edges);
+void parsePorto();
+void parseViseu();
+GraphViewer buildGraphViewer(Graph<Point> & temp_graph);
 
 #endif //EATEXPRESS_PARSE_H
+
